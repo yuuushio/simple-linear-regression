e4924a84523dbe385ea8e4a052bc54f44692efa4
yuuushio-pc <18461232+yuuushio@users.noreply.github.com>
2025-01-17 06:43:29 +1100

    implement algorithm + handle visulization/animation

--- main.py
+++ main.py
@@ -23,10 +23,55 @@ def extract_variables():
     return x, y_vector, df.shape[0]
 
 
-def linear_regression():
-    x_list, yv, n = extract_variables()
-    for i, feature_v in enumerate(x_list):
+def calc_gradients(w, b, xv, yv, n):
+    tmp_w, tmp_b = 0, 0
+    for i in range(n):
+        tmp_w += -2 * xv[i] * (yv[i] - (w * xv[i] + b))
+        tmp_b += -2 * (yv[i] - (w * xv[i] + b))
+
+    return tmp_w / n, tmp_b / n
+
+
+def draw_viz(xv, yv, line_eq, w_list, b_list, animation=False):
+    fig, ax = plt.subplots()
+    ax.plot(xv, yv, "o")
+    (line_plot,) = ax.plot(xv, line_eq, "-")
+
+    ax.set_title("[Food] Delivery Time Prediction")
+    ax.set_xlabel("Distance (km)")
+    ax.set_ylabel("Time Taken to Deliver (Minutes)")
+
+    if animation:
+
+        def update(frame):
+            line_plot.set_ydata(w_list[frame] * xv + b_list[frame])
+            return (line_plot,)
+
+        ani = FuncAnimation(
+            fig, update, frames=range(0, epoch, 2), interval=10, blit=True
+        )
+        ani.save("lr.gif", writer="ffmpeg")
+
+    plt.show()
+
+
+def linear_regression(rate=0.001, epoch=100, animation=False):
+    xv_list, yv, n = extract_variables()
+    for i, feature_v in enumerate(xv_list):
         print(i, feature_v[0], yv[0])
+
+    w, b = 0, 0
+    w_list = []
+    b_list = []
+
+    for _ in range(epoch):
+        tmp_w, tmp_b = calc_gradients(w, b, xv_list[0], yv, n)
+        w += (-1) * rate * tmp_w
+        b += (-1) * rate * tmp_b
+        w_list.append(w)
+        b_list.append(b)
+
+    best_fit_line = w * xv_list[0] + b
 
 
 def main():
